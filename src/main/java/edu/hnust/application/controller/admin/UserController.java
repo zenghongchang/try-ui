b8f07ac116a202a3a05448af63c8b889bb98efa3
zenghongchang <zhc_hnust@outlook.com>
2017-11-19 21:16:21 +0800

    2017-11-19 21:16:18

--- src/main/java/edu/hnust/application/controller/admin/UserController.java
+++ src/main/java/edu/hnust/application/controller/admin/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import edu.hnust.application.common.page.Pager;
 import edu.hnust.application.common.page.ReturnPageData;
 import edu.hnust.application.core.system.StandardPageUtil;
 import edu.hnust.application.orm.user.User;
@@ -28,13 +29,17 @@ public class UserController {
     public String userManage(Model model) {
         standardPageUtil.init(model);
         ReturnPageData<User> data = userService.pageQueryUser(new HashMap<String, Object>());
+        Pager pager = new Pager(1, 20);
         if (data == null) {
             model.addAttribute("userList", new ArrayList<User>());
             model.addAttribute("count", 0L);
+            pager.setTotalRecords(0L);
         } else {
             model.addAttribute("userList", data.getCollection());
             model.addAttribute("count", data.getCollectionCount());
+            pager.setTotalRecords(data.getCollectionCount());
         }
+        model.addAttribute("pager", pager);
         return "management/user_manage";
     }
 }
